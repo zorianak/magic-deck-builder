7d2f481850f9546752a63c0893a699ace4fe29e8
Kim Holmes <github@kimiscool.me>
2014-06-16 21:29:20 -0700

    Views for all card views. header file

--- routes/index.js
+++ routes/index.js
@@ -22,4 +22,69 @@ router.get('/cardlist', function(req, res) {
     });
 });
 
+/* GET Cardlist page. */
+router.get('/black', function(req, res) {
+    var db = req.db;
+    var collection = db.get('creaturecards');
+    collection.find({
+        "black" : "1"
+    },{},function(e,docs){
+        res.render('cardlist', {
+            "cardlist" : docs
+        });
+    });
+});
+
+/* GET Cardlist page. */
+router.get('/blue', function(req, res) {
+    var db = req.db;
+    var collection = db.get('creaturecards');
+    collection.find({
+        "blue" : "1"
+    },{},function(e,docs){
+        res.render('cardlist', {
+            "cardlist" : docs
+        });
+    });
+});
+
+/* GET Cardlist page. */
+router.get('/green', function(req, res) {
+    var db = req.db;
+    var collection = db.get('creaturecards');
+    collection.find({
+        "green" : "1"
+    },{},function(e,docs){
+        res.render('cardlist', {
+            "cardlist" : docs
+        });
+    });
+});
+
+/* GET Cardlist page. */
+router.get('/red', function(req, res) {
+    var db = req.db;
+    var collection = db.get('creaturecards');
+    collection.find({
+        "red" : "1"
+    },{},function(e,docs){
+        res.render('cardlist', {
+            "cardlist" : docs
+        });
+    });
+});
+
+/* GET Cardlist page. */
+router.get('/white', function(req, res) {
+    var db = req.db;
+    var collection = db.get('creaturecards');
+    collection.find({
+        "white" : "1"
+    },{},function(e,docs){
+        res.render('cardlist', {
+            "cardlist" : docs
+        });
+    });
+});
+
 module.exports = router;
