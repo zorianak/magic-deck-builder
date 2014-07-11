b0f58f2da6cf70a26c781716df05b1b21d8f3876
Kim Holmes <github@kimiscool.me>
2014-07-11 07:14:56 -0700

    This had some Backbone stuff, but we aren't doing that in this branch

--- public/javascripts/Card.js
+++ public/javascripts/Card.js
@@ -1,22 +1,10 @@
 // So Creature Cards can be a model basically
 // which would then be a collection of Cards
 
-var CreatureCards = Backbone.Model.extend({
-   defaults: {
-       name: "Not specified",
-       type: "Not specified",
-       power: "0",
-       toughness: "0",
-       cost: "0"
-   },
-    
-    initialize: function() {
-        console.log('Init CreatureCards');
-    }
-});
-
-var Cards = Backbone.Collection.extend({
-   model: CreatureCards 
-});
-
-// So that's a simple way to set this up with Backbone then
+var CreatureCards = function(){
+    this.name = "";
+    this.type = "";
+    this.power = "0";
+    this.toughness = "0";
+    this.cost = "0";
+}
